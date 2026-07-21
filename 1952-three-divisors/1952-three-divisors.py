4352f14c98e062e775a940fe5535c32b87761511
Shaan <shaancitrom123@gmail.com>
2026-07-21 13:22:41 +0530

    Time: 2 ms (71.61%), Space: 12.8 MB (25.13%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.py
+++ 1952-three-divisors/1952-three-divisors.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def isThree(self, n):
         c=0
-        for i in range (2,n):
+        for i in range (1,n+1):
             if n%i==0:
                 c+=1
-        if c==1:
+        if c==3:
             return True
         return False
 
