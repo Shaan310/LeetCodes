5bbd07a0b2900b2004af2d1d7a5c5defb3f02ba7
Shaan <shaancitrom123@gmail.com>
2026-07-21 20:00:03 +0530

    Time: 12 ms (16.27%), Space: 16.9 MB (9.93%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        f=0
+        t=0
+        for i in range(len(bills)):
+            if bills[i] == 5:
+                f+=1
+            elif bills[i]==10:
+                if f>=1:
+                    t+=1
+                    f-=1
+                else:
+                    return False
+            else:
+                if f>=1 and t>=1:
+                    f-=1
+                    t-=1
+                elif (f>=3):
+                    f-=3
+                else:
+                    return False
+        return True
+
+        
+
+
+        
