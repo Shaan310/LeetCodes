a7c9f617fda06896795c0417e679d12ddba155ba
shaan <shaancitrom123@gmail.com>
2026-08-07 23:11:03 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.65%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,4 +1,9 @@
 class Solution(object):
     def toLowerCase(self, s):
-        return s.lower()
-        
+        r = ""
+        for ch in s:
+            if 'A' <= ch <= 'Z':
+                r += chr(ord(ch) + 32)
+            else:
+                r += ch
+        return r
