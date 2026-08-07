014af0053707ea4b4a02b772eaf91a05258c8b17
shaan <shaancitrom123@gmail.com>
2026-08-07 22:01:10 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.67%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -2,16 +2,16 @@ class Solution(object):
     def smallestNumber(self, n, t):
         l = n
         while True:
-            digits = []
+            d = []
             temp = l
             if temp == 0:
-                digits.append(0)   
+                d.append(0)   
             while temp > 0:
-                digits.append(temp % 10)
+                d.append(temp % 10)
                 temp //= 10
             product = 1
-            for digit in digits:
-                product *= digit
+            for dig in d:
+                product *= dig
             if product % t == 0:
                 return l
             l+= 1
