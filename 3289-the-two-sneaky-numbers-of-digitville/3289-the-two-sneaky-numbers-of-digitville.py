67d9dfb4924caa2a2a21b200384a9ab66c938d9a
shaan <shaansuvarna03@gmail.com>
2026-08-15 21:48:30 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.55%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def getSneakyNumbers(self, nums):
-        s={}
+        s=set()
         c=[]
         for n in nums:
             if n in s:
                 c.append(n)
             else:
-                s[n]=1
+                s.add(n)
         return c
 
 
