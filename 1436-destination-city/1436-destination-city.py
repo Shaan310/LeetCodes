5ed6a2610635090c43063244300e39cb211f6e47
shaan <shaansuvarna03@gmail.com>
2026-08-26 13:57:51 +0530

    Time: 0 ms (100%), Space: 12.5 MB (27.99%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -2,10 +2,10 @@ class Solution(object):
     def destCity(self, paths):
         n=len(paths)
         s=set()
-        for i in range(n):
-            s.add(paths[i][0])
-        for i in range(n):
-            if paths[i][1] not in s:
-                return paths[i][1]
+        for a,b in paths:
+            s.add(a)
+        for a,b in paths:
+            if b not in s:
+                return b 
 
         
