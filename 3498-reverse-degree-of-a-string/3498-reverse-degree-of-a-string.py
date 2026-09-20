42cb9ffc34e68ad7d7f2cd59a683ae533a7c5fc5
shaan <shaansuvarna03@gmail.com>
2026-09-20 15:36:17 +0530

    Time: 11 ms (59.78%), Space: 12.5 MB (22.88%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -2,8 +2,9 @@ class Solution(object):
     def reverseDegree(self, s):
         c=0
         for i in range(len(s)):
-            c+=(i+1)*(123-ord(s[i].lower()))
+            c+=(i+1)*(123-ord(s[i]))
         return c
+
 
 
 
